55891324ce2ee80434bcb99ab79ca482c54f27c2
YaroslavUrev <yu.yaroslav.s@gmail.com>
2026-09-14 19:58:57 +0300

    Complete lab 01
    Main lab and additional task (Parameterized tests) done

--- lab01/src/main/java/edu/course/lab01/CourseToolkit.java
+++ lab01/src/main/java/edu/course/lab01/CourseToolkit.java
@@ -1,5 +1,7 @@
 package edu.course.lab01;
 
+import java.util.Arrays;
+
 /**
  * Небольшие методы для первой лабораторной работы.
  */
@@ -14,5 +16,23 @@ public final class CourseToolkit {
      */
     public static boolean isEven(int number) {
         return number % 2 == 0;
+    }
+    public static boolean isPrime(int number) {
+        if (number < 2) {return false;}
+        for(int i = 2; i < number; i++){if (number % i == 0){return false;}}
+        return true;
+    }
+    public static boolean isPalindrome(String text) {
+        if (text == null) {throw new IllegalArgumentException("IllegalArgumentException");}
+        for(int i = 0; i < text.length() / 2; i++){if (text.charAt(i) != text.charAt(text.length() - i - 1)) {return false;}}
+        return true;
+    }
+    public static double average(int[] values) {
+        if (values.length == 0){throw new IllegalArgumentException("IllegalArgumentException");}
+        int ValuesSum = 0;
+        for (int elem:values){
+            ValuesSum += elem;
+        }
+        return (double)ValuesSum / values.length;
     }
 }
--- lab01/src/test/java/edu/course/lab01/CourseToolkitTest.java
+++ lab01/src/test/java/edu/course/lab01/CourseToolkitTest.java
@@ -1,9 +1,11 @@
 package edu.course.lab01;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CourseToolkitTest {
 
@@ -19,5 +21,76 @@ class CourseToolkitTest {
         boolean result = CourseToolkit.isEven(7);
 
         assertFalse(result);
+    }
+
+    @Test
+    void returnsTrueForZero() {
+        boolean result = CourseToolkit.isEven(0);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void returnsTrueForNegativeEvenNumber() {
+        boolean result = CourseToolkit.isEven(-8);
+
+        assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {6, 9})
+    void returnsFalseForPrime(int value) {
+        boolean result = CourseToolkit.isPrime(value);
+
+        Assertions.assertFalse(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 11})
+    void returnsTrueForPrime(int value) {
+        boolean result = CourseToolkit.isPrime(value);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void returnsTrueForPalindrom() {
+        boolean result = CourseToolkit.isPalindrome("ОззО");
+
+        assertTrue(result);
+    }
+
+    @Test
+    void returnsFalseForCaseError() {
+        boolean result = CourseToolkit.isPalindrome("ОзЗО");
+
+        assertFalse(result);
+    }
+
+    @Test
+    void returnsFalseForNull() {
+        assertThrows(IllegalArgumentException.class, () -> CourseToolkit.isPalindrome(null));
+    }
+
+    @Test
+    void returnsPositiveRes() {
+        int[] numbers = {1, 2, 3};
+        double result = CourseToolkit.average(numbers);
+
+        assertEquals(2, result, 1e-9);
+    }
+
+    @Test
+    void returnsNegativeRes() {
+        int[] numbers = {1, -2, -5};
+        double result = CourseToolkit.average(numbers);
+
+        assertEquals(-2, result, 1e-9);
+    }
+
+    @Test
+    void returnsFalseForWrongArgument() {
+        int[] numbers = {};
+        assertThrows(IllegalArgumentException.class, () -> CourseToolkit.average(numbers));
     }
 }
